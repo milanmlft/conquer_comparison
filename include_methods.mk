a86785575a23cc6383deec0a94733de77c78a410
Milan Malfait <milan.malfait94@gmail.com>
2020-08-10 17:02:59 +0200

    Add second method to test run

--- include_methods.mk
+++ include_methods.mk
@@ -3,10 +3,10 @@ empty :=
 space := $(empty) $(empty)
 
 # ## Methods run with R 3.3
-MT3.3 := limmatrend
+MT3.3 := limmatrend voomlimma 
 # edgeRLRT Wilcoxon ttest \
 # SAMseq edgeRQLF BPSC DESeq2 DESeq2nofilt edgeRLRTdeconv \
-# MASTcpm MASTcpmDetRate MASTtpm MASTtpmDetRate SCDE edgeRLRTrobust voomlimma SeuratBimod \
+# MASTcpm MASTcpmDetRate MASTtpm MASTtpmDetRate SCDE edgeRLRTrobust SeuratBimod \
 # SeuratBimodnofilt SeuratBimodIsExpr2 SeuratTobit DESeq2census edgeRLRTcensus monoclecensus \
 # monocle ROTSvoom ROTScpm ROTStpm metagenomeSeq monoclecount \
 # edgeRQLFDetRate DESeq2betapFALSE
