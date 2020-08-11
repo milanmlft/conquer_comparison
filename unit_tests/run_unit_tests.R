8ee5de3e18ba16b28a9630e5f215e20774b43791
Milan Malfait <milan.malfait94@gmail.com>
2020-08-11 17:29:30 +0200

    Update run_unit_tests.R

--- unit_tests/run_unit_tests.R
+++ unit_tests/run_unit_tests.R
@@ -1,7 +1,3 @@
 suppressPackageStartupMessages(library(testthat))
 
-test_results <- test_dir("unit_tests", reporter = "summary")
-
-test_results
-
-sessionInfo()
+test_dir("unit_tests/output_tests", reporter = "summary")
